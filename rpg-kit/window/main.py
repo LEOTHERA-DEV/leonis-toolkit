48a09642d2fae2b95b5f2504ccf0db4c2fc90dbe
Gabi <kayleigh.kasitra@gmail.com>
2025-08-04 17:46:40 +0200

    Setting up project structure

---
+++ rpg-kit/window/main.py
@@ -0,0 +1,11 @@
+import tkinter as tk
+from tkinter import ttk
+
+window_main = tk.Tk()
+
+window_main.title("Leonis RPG KIT | MVP")
+window_main.geometry("860x640")
+window_main.resizable(False, False)
+
+def run_window():
+    window_main.mainloop()
